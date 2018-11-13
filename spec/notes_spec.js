5183973d49961cdfb0274ad3be952e6b93b2a0e7
RyanWolfen7 <wolfen7lada@gmail.com>
2018-11-13 15:05:35 +0000

    added array compare to testing framework" fixes #2

--- spec/notes_spec.js
+++ spec/notes_spec.js
@@ -1,18 +1,13 @@
 const Expect = require("../exponential-testing/lib/Expect")
-const Note = require("../lib/Notes")
+const Notes = require("../lib/Notes")
 
 console.log("Expect to see a list of notes")
-var notes = new Notes;
-notes.notes().push("this")
-notes.notes().push("is")
-notes.notes().push("a")
-notes.notes().push("bunch")
-notes.notes().push("of")
-notes.notes().push("notes")
-var bunchOfNotes = function() {
-  var string = "";
-  for (var i = 0; i < notes.notes.length; i++) {
-    string = string + notes.notes[i];
-  };
-};
-new Expect(string).toEqual("thisisabunchofnotes");
+var notes = new Notes();
+notes.notes.push("this")
+notes.notes.push("is")
+notes.notes.push("a")
+notes.notes.push("bunch")
+notes.notes.push("of")
+notes.notes.push("notes")
+var array = [ 'this', 'is', 'a', 'bunch', 'of', 'notes' ];
+new Expect(notes.listNotes()).compare_array(array);
