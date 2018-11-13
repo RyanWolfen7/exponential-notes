9465dfe9c0546de53841ee801bfcdc469535445e
Todd Langford-Archer <toddpla@gmail.com>
2018-11-13 15:17:36 +0000

    updating notes
    fixes #4

---
+++ spec/fixtures/notes.js
@@ -0,0 +1,14 @@
+const notes = [
+        {
+          'title': 'This is note one',
+          'body': 'This is note one body'
+        }, {
+          'title': 'This is note two',
+          'body': 'This is note two body'
+        }, {
+          'title': 'This is note three',
+          'body': 'This is note three body'
+        }
+      ]
+
+  module.exports = notes;
--- spec/notes_spec.js
+++ spec/notes_spec.js
@@ -1,7 +1,14 @@
-const Expect = require("./expect");
+const expect = require("../tests/expect");
 const Notes = require("../lib/Notes");
+const notesData = require("./fixtures/notes");
 
 console.log("Expects 'Notes.add' to push a note to the notes array");
 var notes = new Notes();
 notes.add("Hello");
-new Expect(notes.notes[0]).toEqual('Hello');
+new expect(notes.notes[0]).toEqual('Hello');
+
+console.log("should update a specific note");
+var notes = new Notes();
+notes.notes = notesData
+notes.update(1, 'updated title', 'changed body')
+new expect(notes.notes[1].title).toEqual('updated title')
