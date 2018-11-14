87872114eea123c3af6941f0550a68fc6bdeeef6
NaomiBid <naomi0bid@gmail.com>
2018-11-14 15:45:00 +0000

    added 'add note' buttom to index view

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function() {
   console.log("here")
   document.getElementById("app").innerHTML = NavBar().render();
+  document.getElementById("app").innerHTML = Index().render();
 }, false)
