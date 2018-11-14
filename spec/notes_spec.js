3d0b236f69828ff9d3dc4716d97f5d46c467c8eb
Rupert Adams <RupertNCAdams@aol.com>
2018-11-14 09:38:31 +0000

    added a find button for specific notes

--- spec/notes_spec.js
+++ spec/notes_spec.js
@@ -1,33 +1,11 @@
-const Expect = require("../exponential-testing/lib/Expect")
-const Notes = require("../lib/Notes")
-const notesData = require("./fixtures/notes");
-
-console.log("Expect to see a list of notes")
-var notes = new Notes();
-notes.notes.push("this")
-notes.notes.push("is")
-notes.notes.push("a")
-notes.notes.push("bunch")
-notes.notes.push("of")
-notes.notes.push("notes")
-var array = [ 'this', 'is', 'a', 'bunch', 'of', 'notes' ];
-new Expect(notes.listNotes()).compare_array(array);
-
+const Expect = require("../exponential-testing/lib/Expect");
+const Notes = require("../lib/Notes");
 
 console.log("Expects 'Notes.add' to push a note to the notes array");
 var notes = new Notes();
 notes.add("Hello");
 new Expect(notes.notes[0]).toEqual('Hello');
 
-
-console.log("should update a specific note");
-var notes = new Notes();
-notes.notes = notesData
-notes.update(1, 'updated title', 'changed body')
-new Expect(notes.notes[1].title).toEqual('updated title')
-
-console.log("should delete note from notes");
-notes = new Notes();
-notes.notes = [1,2,3]
-notes.delete(2)
-new Expect(notes.notes.length).toEqual(2)
+console.log("Expects 'Notes.find' to find a specific note from the array");
+notes.add("World");
+new Expect(notes.find(0)).toEqual("Hello");
