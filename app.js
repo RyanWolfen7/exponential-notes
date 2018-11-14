278b68a413855e85b91e3c07210226e524319f9d
NaomiBid <naomi0bid@gmail.com>
2018-11-14 15:51:59 +0000

    included list_notes in index.html script tag

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 window.addEventListener("load", function() {
   console.log("here")
-  document.getElementById("app").innerHTML = NavBar().render();
   document.getElementById("app").innerHTML = Index().render();
 }, false)
