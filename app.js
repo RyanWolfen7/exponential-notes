fd76edb1115cc5171d9b55d7c3c765f473ddf9ea
shannongamby <shannongamby@hotmail.com>
2018-11-15 15:54:10 +0000

    suggested changes made

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ window.addEventListener("load", function() {
 const handleNoteDetailPage = function(index) {
   document.getElementById("app").innerHTML = DetailPage().render(index);
 }
+
+const editNotePage = function(index) {
+  document.getElementById("app").innerHTML = NotesEditPageView().render(index);
+}
