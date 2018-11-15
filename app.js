e02e40646ff86393608a036075296c9f0678525b
becc-mu <becc.mulugeta@gmail.com>
2018-11-15 17:04:36 +0000

    added update note handler

--- app.js
+++ app.js
@@ -9,5 +9,10 @@ const handleNoteDetailPage = function(index) {
 }
 
 const handleEditNotePage = function(index) {
-  document.getElementById("app").innerHTML = NotesEditPageView().render(notePad.listNotes()[index]);
+  document.getElementById("app").innerHTML = NotesEditPageView().render(notePad.listNotes()[index], index);
+}
+
+const updateNote = function(index, title, body) {
+  notePad.update(index, title, body);
+  document.getElementById("app").innerHTML = DetailPage().render(index);
 }
